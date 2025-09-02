5c2944c2ed7887eb9d30003a4d13e887e85c6bd7
Inggar Kinanthi <inggarkinanthi@example.com>
2025-09-02 19:09:15 +0700

    Tambahkan LazyColumn menampilkan DataSiswa.

---
+++ app/src/main/java/com/example/pamhttp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.pamhttp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pamhttp.ui.theme.PamhttpTheme
+import com.example.pamhttp.uicontroller.DataSiswaApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PamhttpTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DataSiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PamhttpTheme {
+        Greeting("Android")
+    }
+}
