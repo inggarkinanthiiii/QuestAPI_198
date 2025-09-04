2e4d97e55706119eb623a2132a643bc57a8059dd
Inggar Kinanthi <inggarkinanthi@example.com>
2025-09-04 17:09:15 +0700

    menambahkan data class dan fungsi uiState dan detail siswa

--- app/src/main/java/com/example/pamhttp/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/pamhttp/modeldata/DataSiswa.kt
@@ -4,8 +4,35 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class DataSiswa(
-    val id: String,
-    val nama: String,
-    val alamat: String,
-    val telpn: String
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val telpn : String
+)
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa (
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpn: String = "",
+)
+fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa (
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpn = telpn,
+)
+fun DataSiswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa (
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa (
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpn = telpn
 )
