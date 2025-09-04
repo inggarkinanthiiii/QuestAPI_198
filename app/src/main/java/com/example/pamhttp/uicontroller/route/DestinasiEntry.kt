5005f941b1e1af89ae63f086eff907446bb314cf
Inggar Kinanthi <inggarkinanthi@example.com>
2025-09-04 17:11:28 +0700

    membuat destinasi entry berisi route dan title

---
+++ app/src/main/java/com/example/pamhttp/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.pamhttp.uicontroller.route
+
+import com.example.pamhttp.R
+import com.example.pamhttp.uicontroller.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
