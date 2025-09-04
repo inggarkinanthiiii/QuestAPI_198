e4f1ea858f4ad45284015adfafaa64a276f96fce
Inggar Kinanthi <inggarkinanthi@example.com>
2025-09-04 17:17:55 +0700

    membuat halaman file DestinasiHome.kt

---
+++ app/src/main/java/com/example/pamhttp/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.pamhttp.uicontroller.route
+
+import com.example.pamhttp.R
+import com.example.pamhttp.uicontroller.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
