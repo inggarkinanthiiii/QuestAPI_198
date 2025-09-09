6199b4dd71014eed3590627101fb2424884ae228
Inggar Kinanthi <inggarkinanthi@example.com>
2025-09-09 17:06:09 +0700

    menambahkan navigasi untuk atau menuju ke halaman detail

--- app/src/main/java/com/example/pamhttp/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/pamhttp/uicontroller/PetaNavigasi.kt
@@ -3,11 +3,15 @@ package com.example.pamhttp.uicontroller
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pamhttp.uicontroller.route.DestinasiDetail
 import com.example.pamhttp.uicontroller.route.DestinasiEntry
-import com.example.pamhttp.view.DestinasiHome
+import com.example.pamhttp.uicontroller.route.DestinasiHome
+import com.example.pamhttp.view.DetailSiswaScreen
 import com.example.pamhttp.view.EntrySiswaScreen
 import com.example.pamhttp.view.HomeScreen
 
@@ -26,10 +30,20 @@ fun HostNavigasi(
     NavHost(navController = navController, startDestination = DestinasiHome.route,
         modifier = Modifier) {
         composable(DestinasiHome.route) {
-            HomeScreen(navigateToItemEntry = {navController.navigate(DestinasiEntry.route)})
+            HomeScreen(navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}")
+                })
         }
         composable (DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = {navController.navigate(DestinasiHome.route)})
+        }
+        composable (DestinasiDetail.routeWithArgs, arguments = listOf(navArgument(DestinasiDetail.itemIdArgs){
+            type = NavType.IntType })
+        ){
+            DetailSiswaScreen(
+                navigateBack = {navController.navigate(DestinasiHome.route)}
+            )
         }
     }
 }
