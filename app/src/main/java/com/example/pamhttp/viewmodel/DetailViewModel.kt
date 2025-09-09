8ced22291d071f0bb9e3e612c9d8cd2ef7411bfa
Inggar Kinanthi <inggarkinanthi@example.com>
2025-09-09 16:54:07 +0700

    Kode ini adalah ViewModel di arsitektur MVVM untuk mengatur UI state detail siswa dengan data dari repository (RepositoryDataSiswa)

---
+++ app/src/main/java/com/example/pamhttp/viewmodel/DetailViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.pamhttp.viewmodel
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mydatasiswa.modeldata.DataSiswa
+import com.example.pamhttp.repositori.RepositoryDataSiswa
+import com.example.pamhttp.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+import retrofit2.Response
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa : DataSiswa) : StatusUIDetail
+    object Loading : StatusUIDetail
+    object Error : StatusUIDetail
+
+}
+
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArgs])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+    init {
+        getSatuSiswa()
+    }
